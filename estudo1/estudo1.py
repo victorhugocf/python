6590d93c62d99421c687c820c3403fddf57d21af
Victor Hugo <victorhugo.13cf@gmail.com>
2025-05-11 20:28:47 -0300

    Update estudo1.py

--- estudo1/estudo1.py
+++ estudo1/estudo1.py
@@ -1,13 +1,12 @@
-#Escape
-print("Victor \"Hugo\"")
+prim_val = input('Digite o primeiro valor: ')
+segund_val = input('Digite o segundo valor: ')
 
-#r
-print(r"Victor \"Hugo\"")
+int_prim_val = int(prim_val)
+int_segund_val = int(segund_val)
 
-#end
-print("Victor", end=' ')
-print("Hugo")
-
-#sep
-print("Victor", "Hugo", sep='-')
-
+if int_prim_val > int_segund_val:
+    print(f'O primeiro valor {int_prim_val} e maior que o segundo {int_segund_val}.')
+elif int_prim_val == int_segund_val:
+    print('Os dois valores sao iguais.')
+else:
+    print(f'O segundo valor {int_segund_val} e maior que o primeiro {int_prim_val}.')
