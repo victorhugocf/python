c45909d981784f91169beeb9b7057a8f7b48827f
Victor Hugo <victorhugo.13cf@gmail.com>
2025-05-12 20:43:49 -0300

    Update pratica_impar_par.py

--- pratica/pratica_impar_par.py
+++ pratica/pratica_impar_par.py
@@ -3,9 +3,9 @@ numero = input('Digite um número inteiro: ')
 try:
     numero_int = int(numero)
     if numero_int % 2 == 0:
-        print('O número digitado é um número par!')
+        print(f'O número {numero_int} é um número par!')
     else:
-        print('O número digitado é um número ímpar!')
+        print(f'O número {numero_int} é um número ímpar!')
 except:
     print('Digite um número inteiro válido!')
 
