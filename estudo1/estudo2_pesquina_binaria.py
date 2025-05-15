b24c18c25530792d872a99a93d06517651ba4a4d
Victor Hugo <victorhugo.13cf@gmail.com>
2025-05-15 19:40:08 -0300

    Update estudo2_pesquina_binaria.py

--- estudo1/estudo2_pesquina_binaria.py
+++ estudo1/estudo2_pesquina_binaria.py
@@ -1,34 +1,27 @@
 def pesquisa_binaria(lista, item):
     baixo = 0
-    alto = len(lista) - 1
+    alto = len(lista) - 1 #Determina a posição mais alta do array.
 
     while baixo <= alto:
-        meio = (baixo + alto) // 2
+        meio = (baixo + alto) // 2 #Encontra a posição no meio(arredodando se o número for impar) do array.
         chute = lista[meio]
-        if chute == item:
+        if chute == item: #Se este for o número certo retorna a posição.
             return meio
-        elif chute > item:
+        elif chute > item: #O chute foi muito alto.
             alto = meio - 1
-        else:
+        else: #O chute foi muito baixo.
             baixo = meio + 1
     
-    return None
+    return None #A posição não foi encontrada.
 
 import random
 
-minhaLista = list(range(1, 16))
-i = 1
-while i <= 15:
-    minhaLista[i] = random.randint(1, 100)
-    i += 1
+minhaLista = [45, 57, 83, 90, 155, 233, 257, 572, 663, 899, 913, 1000]
 
 num = random.choice(minhaLista)
 
 pos = pesquisa_binaria(minhaLista, num)
 
-print(f'O número {num} está no array na posição {pos}!')
-print('ARRAY:')
+print(minhaLista)
 
-while i <= 15:
-    print(minhaLista[i])
-    i += 1
+print(f'O número {num} está no array na posição {pos}!')
