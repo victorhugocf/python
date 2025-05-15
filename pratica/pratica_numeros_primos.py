b8b6d8f021d52e0f23f3aa3edac7968f11583969
Victor Hugo <victorhugo.13cf@gmail.com>
2025-05-14 22:40:08 -0300

    Update pratica_numeros_primos.py

--- pratica/pratica_numeros_primos.py
+++ pratica/pratica_numeros_primos.py
@@ -11,15 +11,22 @@ def verificaPrimo(x):
 
 num = input('Digite um número: ')
 
-if num.isdigit:
-    num = int(num)
+verifica = 1
+while verifica:
+    if num.isdigit():
+        num = float(num)
+        verifica = 0
+    else:
+        print('Esse número é inválido, digite uma entrada válida')
+        num = input('Digite um número: ')
+
 
 contPrimo = verificaPrimo(num)
 cont = 1
 
 
 if contPrimo == 2:
-    print(f'O {num} é um número primo, esses são os numeros primos antes dele são:')
+    print(f'O {num:.0f} é um número primo, esses são os numeros primos antes dele são:')
     while cont < num:
         contPrimo = verificaPrimo(cont)
         if contPrimo == 2:
@@ -28,7 +35,7 @@ if contPrimo == 2:
         else:
             cont += 1
 else:
-    print(f'O {num} não é um número primo, esses são os numeros primos antes dele são:')
+    print(f'O {num:.0f} não é um número primo, esses são os numeros primos antes dele são:')
     while cont < num:
         contPrimo = verificaPrimo(cont)
         if contPrimo == 2:
