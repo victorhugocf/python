189e8d1f49aebf26076e6c0ff575159ac9e486da
Victor Hugo <victorhugo.13cf@gmail.com>
2025-06-06 16:01:19 -0300

    Update estudo3_quicksort.py

--- estudo/estudo3_quicksort.py
+++ estudo/estudo3_quicksort.py
@@ -1,15 +1,15 @@
 import random
 
 def quicksort(lista):
-    if len(lista) < 2:
+    if len(lista) < 2: #Caso Base.
         return lista
     else:
-        pivo = lista[len(lista) // 2]
-        menores = [i for i in lista[1:] if i <= pivo]
-        maiores = [i for i in lista[1:] if i > pivo]
-        return quicksort(menores) + pivo + quicksort(maiores)
+        pivo = lista[0] #Caso recursivo.
+        menores = [i for i in lista[1:] if i <= pivo] #Array secundário com os números menores que o pivô.
+        maiores = [i for i in lista[1:] if i > pivo] #Array secundário com os números maiores que o pivô.
+        return quicksort(menores) + [pivo] + quicksort(maiores)
 
-lista = random.sample(range(1, 100), 30) #Gerar uma lista com 30 índices aleatórios (sem pretições).
+lista = random.sample(range(1, 100), 10) #Gerar uma lista com 30 índices aleatórios (sem pretições).
 
 nova_lista = quicksort(lista)
 
