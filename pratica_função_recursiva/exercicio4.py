f11725477e41df92220f419015673f5e030d3c0e
Victor Hugo <victorhugo.13cf@gmail.com>
2025-06-06 16:01:22 -0300

    Create exercicio4.py

---
+++ pratica_função_recursiva/exercicio4.py
@@ -0,0 +1,20 @@
+#Escreva uma função em Python que implemente o algoritmo QuickSort de forma recursiva.
+#Use o último elemento do array como pivô.
+#A função deve retornar um novo array ordenado, sem modificar o original.
+
+import random
+
+def quicksort(lista):
+    if len(lista) < 2:
+        return lista #Caso base
+    else:
+        pivo = lista[len(lista) - 1]
+        menores = [i for i in lista[1:] if i <= pivo]
+        maiores = [i for i in lista[1:] if i > pivo]
+
+        return quicksort(menores) + [pivo] + quicksort(maiores)
+    
+lista = random.sample(range(1, 50), 10)
+
+print(lista)
+print(quicksort(lista))
