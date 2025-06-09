bbce7dfae97b167418decc9167a85b269ac6202d
Victor Hugo <victorhugo.13cf@gmail.com>
2025-06-09 20:10:28 -0300

    Update pratica_funcao1.py

--- pratica/pratica_funcao1.py
+++ pratica/pratica_funcao1.py
@@ -3,17 +3,18 @@
 def search_prefix(prefix):
     def in_word(arr):
         in_word_arr = []
-        for i in arr:
+        for i in range(0, len(arr)):
             if prefix in arr[i]:
-                in_word_arr.append(word)
-                return in_word_arr
+                in_word_arr.append(arr[i])
+        return in_word_arr
     return in_word
 
 arr = []
 
 prefix = search_prefix(input('Insira o prefixo que deseja procurar: '))
+number_of_words = input('Quantas palavras deseja procurar esse prefixo? ')
 
-for i in range(1, 4):
+for i in range(1, (number_of_words - 1)):
     word = input('Digite uma palavra: ')
     arr.append(word)
 
